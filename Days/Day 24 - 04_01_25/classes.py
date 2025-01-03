1a013391ac477f5ab7ef9b5d600d37c385cc9ed3
Simon A. Betts <bettsy-boy10@hotmail.co.uk>
2025-01-03 16:13:17 +0000

    Day 23 - HTML

---
+++ Days/Day 24 - 04_01_25/classes.py
@@ -0,0 +1,14 @@
+class Trooper(object):
+
+	"""docstring for Trooper"""
+
+	def __init__(self, arg):
+		super(Trooper, self).__init__()
+		self.arg = arg
+		
+class Droid(object):
+	"""docstring for Droid"""
+	def __init__(self, arg):
+		super(Droid, self).__init__()
+		self.arg = arg
+		
